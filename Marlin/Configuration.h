0cf9f0f643c5639ff62ea5db5aad6825dfe04d3d
jonylentzmc <76884929+jonylentzmc@users.noreply.github.com>
2025-05-27 14:50:37 -0300

    Update Configuration.h
    stupid mistake?

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1054,7 +1054,7 @@
 //#define USE_WMAX_PLUG
 
 // Enable pullup for all endstops to prevent a floating state
-#define ENDSTOPPULLUPS
+//#define ENDSTOPPULLUPS
 #if DISABLED(ENDSTOPPULLUPS)
   // Disable ENDSTOPPULLUPS to set pullups individually
   #define ENDSTOPPULLUP_XMIN
